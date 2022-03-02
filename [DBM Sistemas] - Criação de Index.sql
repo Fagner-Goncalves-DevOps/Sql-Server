a98af270ca067ec570da471f8a1bceead3a8824c
fagner gonçalves <fagnergoncalves15@gmail.com>
2022-03-01 23:54:16 -0300

    Remoções desnecessarias

--- [DBM Sistemas] - Criação de Index.sql
+++ [DBM Sistemas] - Criação de Index.sql
@@ -2,6 +2,7 @@
 ------------------------------------------------------------------------------------
 /*
 Create:Fagner Gonçalves 
+Alterado por : --
 */
 ------------------------------------------------------------------------------------
 USE [TELECOM]
@@ -19,14 +20,3 @@ WITH (FILLFACTOR = 90, DATA_COMPRESSION = PAGE) --ON PRIMARY]
 
 --WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON DW_VIVO_ACIONAMENTOS
 
-GO
-
-
-
-		T.NOM_TIP,
-		T.DES_STAT,
-SELECT TOP 10 * FROM TAB_ESTEIRA_TELECOM_TP
-
-USE [TELECOM]
-GO
-CREATE NONCLUSTERED INDEX [<Name of Missing Index, sysname,>] ON [dbo].[TAB_ESTEIRA_TELECOM_TP] ([DATA])
